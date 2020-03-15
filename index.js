d14158e987e3f04b4a9dc4dd774f81ff2bd32044
nick chen <nick.chen.pro@gmail.com>
2020-03-15 01:22:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var newArray = [];
+  var num = 0;
+  while ( num < facts.length) {
+    newArray.push(`${facts[num]}!!!`);
+    num++;
+  }
+  return newArray;
+}
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
+}
